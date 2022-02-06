f63187f47f9be4afc3a12444b7d5567b0c7a67b9
Andrey Velikoredchanin <andy@andyhost.ru>
2022-02-06 15:07:43 +0300

    BCL-1114: Test wrappers

--- gsn/test/TestForward.go
+++ gsn/test/TestForward.go
@@ -28,9 +28,21 @@ type TestForwardForwardRequest struct {
 var __contractTestForwardSourceABI = "[{\"inputs\":[{\"internalType\":\"address\",\"name\":\"_forwarder\",\"type\":\"address\"}],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"caller\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"val\",\"type\":\"uint256\"}],\"name\":\"FooCalled\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_val\",\"type\":\"uint256\"},{\"internalType\":\"string\",\"name\":\"_str\",\"type\":\"string\"}],\"name\":\"foo\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"forwarder\",\"type\":\"address\"}],\"name\":\"isTrustedForwarder\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"sender\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"str\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"components\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"gas\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"nonce\",\"type\":\"uint256\"},{\"internalType\":\"bytes\",\"name\":\"data\",\"type\":\"bytes\"}],\"internalType\":\"structTestForward.ForwardRequest\",\"name\":\"req\",\"type\":\"tuple\"},{\"internalType\":\"bytes32\",\"name\":\"domainSeparator\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"requestTypeHash\",\"type\":\"bytes32\"},{\"internalType\":\"bytes\",\"name\":\"suffixData\",\"type\":\"bytes\"},{\"internalType\":\"bytes\",\"name\":\"sig\",\"type\":\"bytes\"}],\"name\":\"testExecute\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"success\",\"type\":\"bool\"},{\"internalType\":\"string\",\"name\":\"ret\",\"type\":\"string\"}],\"stateMutability\":\"payable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"trustedForwarder\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"val\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"versionRecipient\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]"
 
 func Foo(gsnParams *gsn.CallOpts, args ...interface{}) (txHash common.Hash, err error) {
-	return gsn.Executor(gsnParams, __contractTestForwardSourceABI, "foo", args...)
+	gsnCallParams := &wrappers.GsnCallOpts{
+		GsnCaller:       gsnParams.GsnCaller,
+		ChainId:         gsnParams.ChainId,
+		Signer:          gsnParams.Signer,
+		ContractAddress: gsnParams.ContractAddress,
+	}
+	return wrappers.GsnExecutor(gsnCallParams, __contractTestForwardSourceABI, "foo", args...)
 }
 
 func TestExecute(gsnParams *gsn.CallOpts, args ...interface{}) (txHash common.Hash, err error) {
-	return gsn.Executor(gsnParams, __contractTestForwardSourceABI, "testExecute", args...)
+	gsnCallParams := &wrappers.GsnCallOpts{
+		GsnCaller:       gsnParams.GsnCaller,
+		ChainId:         gsnParams.ChainId,
+		Signer:          gsnParams.Signer,
+		ContractAddress: gsnParams.ContractAddress,
+	}
+	return wrappers.GsnExecutor(gsnCallParams, __contractTestForwardSourceABI, "testExecute", args...)
 }
